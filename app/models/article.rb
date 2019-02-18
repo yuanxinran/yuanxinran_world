0adbd157c0866769d6e6bd94a39c8ad2e1928fd4
Calvin Lui <clui@cmu.edu>
2019-02-18 12:20:28 -0500

    added validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
-  belongs_to :category
+  has_one :category
+
+  validates_presence_of :title
+  validates_presence_of :content
 
 	scope :active, 			 -> { where('active = ?', true) }
   scope :alphabetical, -> { order('title') }
